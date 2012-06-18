4dcda19ca9610bd8ea0c4c65f9a1cf13fb8a25f4
Stephen Moore <stephen@delfick.com>
2012-06-18 17:57:37 +1000

    pedantic

--- tests/test_decryption.py
+++ tests/test_decryption.py
@@ -9,10 +9,8 @@ import unittest
 describe "decryption":
     it "works with with small data":
         message = Message(data.get_rsa_keys(), data.get_encrypted('small'))
-        message.decrypt()
-        self.assertEqual(message.plaintext, data.get_original('small'))
+        self.assertEqual(message.decrypt(), data.get_original('small'))
 
     it "works with big data":
         message = Message(data.get_rsa_keys(), data.get_encrypted('big'))
-        message.decrypt()
-        self.assertEqual(message.plaintext, data.get_original('big'))
+        self.assertEqual(message.decrypt(), data.get_original('big'))
