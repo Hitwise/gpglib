3e2ed40895b68c81ed70139ad91681d840608fc0
David Zderic <david.zderic@hitwise.com>
2012-06-18 12:38:01 +1000

    Finished session key decryption and data decryption
    * Now properly PKCS1-decoding the encrypted session key
    * Created a dodgy mapping of cipher types -> cipher implementations
    * Finished the `SymEncryptedParser` class implementation

--- gpglib/content_parsers.py
+++ gpglib/content_parsers.py
@@ -1,5 +1,9 @@
-from Crypto.Cipher import CAST
 import bitstring
+from Crypto import Random
+from Crypto.Cipher import CAST, PKCS1_v1_5
+from Crypto.Hash import SHA
+
+import errors
 
 class ContentParser(object):
     """Delegator to the different content parsers"""
@@ -74,19 +78,73 @@ class PubSessionKeyParser(Parser):
         # Read the encrypted session key
         encrypted_session_key = self.parse_mpi(region).bytes
         
-        # Decrypt the session key
-        session_key = key.decrypt(encrypted_session_key)
+        # The encrypted session key is EME-PKCS1-encoded (as described in section
+        # 13.1 of the RFC). The sentinel value is generated for crypto reasons,
+        # and it is insecure to directly compare it to the output. We must only ever
+        # compare the checksum of the resulting key.
+        pkcs = PKCS1_v1_5.new(key)
 
-        # Pass the session key on to the next parser
+        # Generate the sentinel value (19 is the exact length of a valid decrypted
+        # passphrase)
+        sentinel = Random.new().read(19)
+
+        # Decrypt and decode the session key
+        decrypted = pkcs.decrypt(encrypted_session_key, sentinel)
+        padded_session_key = bitstring.ConstBitStream(bytes=decrypted)
+
+        # The algorithm used to encrypt the message is the first byte
+        algo = padded_session_key.read(8).uint
+
+        # The session key is the next 16 bytes
+        session_key = padded_session_key.read(16*8).bytes
+
+        # The checksum is the last two bytes
+        checksum = padded_session_key.read(2*8).uint
+
+        # Generate a checksum from the session_key (section 5.1 in the RFC). This
+        # involves summing up all the bytes of the session key and `mod`ing it
+        # by 65536.
+        generated_checksum = sum(ord(i) for i in session_key) % 65536
+
+        # Compare the checksums and throw an error if they don't match
+        if checksum != generated_checksum:
+            raise errors.PGPException("The decrypted session key was invalid (checksums didn't match)")
+
+        # Pass the session key bytes and the algorithm used on to the next parser
         return {
+            'algo': algo,
             'session_key': session_key,
         }
         
 class SymEncryptedParser(Parser):
     """Parse symmetrically encrypted data packet"""
-    def consume(self, tag, message, region, session_key):
-        iv_len = 8*(CAST.block_size+2)
-        iv = region.read(iv_len).bytes
-        ciphertext = region.read(region.len - iv_len).bytes
-        cipher = CAST.new(session_key, CAST.MODE_OPENPGP, iv)
-        message.decrypted = cipher.decrypt(ciphertext)
+    # Mapping of PGP encryption algorithm types to a PyCrypto module which implements
+    # that particular algorithm
+    ENCRYPTION_ALGORITHMS = {
+        3: CAST,  # CAST5
+    }
+
+    def consume(self, tag, message, region, algo, session_key):
+        # Get the encryption algorithm used
+        try:
+            cipher = self.ENCRYPTION_ALGORITHMS[algo]
+        except KeyError:
+            raise NotImplementedError("Symmetric encryption type '%d' hasn't been implemented" % algo)
+
+        # Handy alias for the encryption algo's block size
+        block_size = cipher.block_size
+
+        # Find out the length of the IV
+        iv_len = block_size + 2
+
+        # Read in the encrypted IV
+        iv = region.read(8*iv_len).bytes
+
+        # The ciphertext is what's left in `region`
+        ciphertext = region.read('bytes')
+
+        # Build the cipher object from the session key and the IV
+        decryptor = cipher.new(session_key, cipher.MODE_OPENPGP, iv)
+
+        # Decrypt the ciphertext and set it on the class
+        message.decrypted = decryptor.decrypt(ciphertext)
